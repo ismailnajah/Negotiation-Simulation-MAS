dccec124b6ef8428828dd106fc824ca1040fe81e
ismailnajah <for2hassan@gmail.com>
2019-04-05 05:36:19 +0100

    --test of git

--- src/Agents/Gestionnaire.java
+++ src/Agents/Gestionnaire.java
@@ -3,4 +3,13 @@ package Agents;
 public class Gestionnaire {
     private String fname;
     private String lname;
+    private String login;
+    private String password;
+
+    public Gestionnaire(String fname, String lname, String login, String password) {
+        this.fname = fname;
+        this.lname = lname;
+        this.login = login;
+        this.password = password;
+    }
 }
